f2342128a7919d62239eca3c60591aeb72239889
dan4ik58620 <56477594+dan4ik58620@users.noreply.github.com>
2020-10-30 19:58:50 +0200

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// const total = 100;
+// let ordered = 150 ;
+
+// if (ordered > total) {
+//     console.log("На складе не достаточно товаров")
+// } else {
+//     console.log("Заказ оформлен с вами свяжется менеджер")
+// }
+
+//  ordered = 100 ;
+
+// if (ordered > total) {
+//     console.log("На складе не достаточно товаров")
+// } else {
+//     console.log("Заказ оформлен с вами свяжется менеджер")
+// }
+
+// ordered = 50 ;
+
+// if (ordered > total) {
+//     console.log("На складе не достаточно товаров")
+// } else {
+//     console.log("Заказ оформлен с вами свяжется менеджер")
+// }
+
+
+
